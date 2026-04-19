7f02ebea79fc2a6de0cfdf6bf8fd5eb05b7bfd24
suhaifa14 <sarasubah14@gmail.com>
2026-04-20 01:41:19 +0600

    Update(whitenoise)settings.py

--- bloodbank/bloodbank/settings.py
+++ bloodbank/bloodbank/settings.py
@@ -127,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
